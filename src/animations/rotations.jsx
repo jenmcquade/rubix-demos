8f550769d8c2b3d13b7a31fc07b3bcb20730cb67
Jonathan McQuade <jonathan@bistorm.associates>
2018-01-15 12:29:56 -0800

    Styling fixes across device sizes

---
+++ src/animations/rotations.jsx
@@ -0,0 +1,47 @@
+import { keyframes } from 'styled-components';
+
+export default class Rotations {
+  constructor() {
+    this.simpleRotateX = keyframes`
+        0% {
+          transform: rotateX(0);
+        }
+        
+        50% {
+          transform: rotateX(360deg)
+        }
+        
+        100% {
+          transform: rotateX(0);
+        }
+    `;
+
+    this.simpleRotateY = keyframes`
+      0% {
+        transform: rotateY(0);
+      }
+      
+      50% {
+        transform: rotateY(360deg)
+      }
+      
+      100% {
+        transform: rotateY(0);
+      }
+    `;
+
+    this.spin = keyframes`
+      0% {
+        transform: rotateY(0) rotateX(0);
+      }
+
+      50% {
+        transform: rotateY(760deg) rotateX(760deg);
+      }
+      
+      100% {
+        transform: rotateY(0) rotateX(0);
+      }
+    `;
+  }
+}
