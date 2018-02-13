53040ea54b7f3b88b4feeb547e575d664b1dc32c
Jonathan <jonathan@bistorm.org>
2018-02-13 19:32:40 +0000

    Updates for build releases using public_build.sh

--- public_build.sh
+++ public_build.sh
@@ -2,13 +2,38 @@
 #
 # Description: Generates a Production Docker container,
 #   then copies the o3dv container build files to ./public_build
+#   then pushes newest instaproxy image to Heroku
+#   then commits newest public_build folder to GitHub on master branch
+#     which then triggers a Heroku build of the master branch
+#   then returns to the original branch
 #
+
+docker-compose -f docker-compose.prod.yml down
+
+date=`date +'%y.%m.%d %H:%M:%S'`
 
 docker-compose -f docker-compose.prod.yml up --build -d
 
 docker cp o3dv:/build/. ./public_build/
 
 docker-compose -f docker-compose.prod.yml down
+
+heroku container:login
+
+docker tag instaproxy registry.heroku.com/igdata/web
+docker push registry.heroku.com/igdata/web
+
+original_branch=`git branch | grep \*`
+
+git checkout master
+
+git pull origin master
+
+git commit -m "Production Release at $date" ./public_build ./docker-compose.prod.yml 
+
+git push origin master
+
+git checkout $original_branch
 
 exit 0
 
